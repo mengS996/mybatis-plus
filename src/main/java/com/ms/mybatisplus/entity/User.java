36034e9b618b18a9ec05aa07a6556ce8bc184c9f
ms <715289580@qq.com>
2021-06-11 17:07:23 +0800

    dev commit

--- src/main/java/com/ms/mybatisplus/entity/User.java
+++ src/main/java/com/ms/mybatisplus/entity/User.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 /**
  * @author     ：孟老师
  * @date       ：Created in 2021/5/26 11:08
@@ -21,4 +23,6 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
 }
