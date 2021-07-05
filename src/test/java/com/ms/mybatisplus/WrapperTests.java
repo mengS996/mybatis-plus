e798b8ee1c8162b12febe131e7927ea981bba44f
ms <715289580@qq.com>
2021-07-05 18:10:26 +0800

    条件构造器

--- src/test/java/com/ms/mybatisplus/WrapperTests.java
+++ src/test/java/com/ms/mybatisplus/WrapperTests.java
@@ -1,13 +1,19 @@
 package com.ms.mybatisplus;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.ms.mybatisplus.entity.User;
 import com.ms.mybatisplus.mapper.UserMapper;
+import net.minidev.json.writer.UpdaterMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author     ：孟老师
@@ -66,6 +72,7 @@ public class WrapperTests {
     public void test4() {
         //组装查询条件，修改条件
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        //i代表的就是queryWrapper
         queryWrapper
                 .like("name","n")
                 .and(i -> i.lt("age",18).or().isNull("email"));
@@ -77,4 +84,119 @@ public class WrapperTests {
         System.out.println(result);
     }
 
+    /**
+     * 查询所有用户的用户名和年龄
+     */
+    @Test
+    public void test5() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("name","age");
+        //selectMaps()返回Map集合列表，通常配合select()使用，避免User对象中没有被查询到的列值为null
+        //如果使用selectList()则会出现列为null的情况
+        List<Map<String, Object>> maps = userMapper.selectMaps(queryWrapper);//返回值是Map列表
+        maps.forEach(System.out::println);
+    }
+
+    /**
+     * 查询id不大于3的所有用户的id列表
+     */
+    @Test
+    public void test6() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.inSql("id", "select id from user where id <= 3");
+        //selectObjs的使用场景：只返回一列
+        List<User> users = userMapper.selectList(queryWrapper);//返回值是Object列表
+        users.forEach(System.out::println);
+    }
+    /**
+     * 条件的优先级: 需求同Test4.在test4的基础上修改
+     * 查询名字中包含n，且（年龄小于18或email为空的用户），并将这些用户的年龄设置为18，邮箱设置为 user@ms.com
+     */
+    @Test
+    public void test7() {
+        //组装set子句
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        //i代表的就是updateWrapper
+        updateWrapper
+                .set("age",18)
+                .set("email","user@ms.com")
+                .like("name","n")
+                .and(i -> i.lt("age",18).or().isNull("email"));
+        //这里必须要创建User对象，否则无法应用自动填充。如果没有自动填充，可以设置为null
+        User user = new User();
+        int result = userMapper.update(user, updateWrapper);
+        System.out.println(result);
+    }
+
+    /**
+     * 查询名字中包含n，年龄大于10且小于20的用户，查询条件来源于用户输入，是可选的
+     */
+    @Test
+    public void test8() {
+        //定义查询条件，有可能为null（用户未输入）
+        String name = null;
+        Integer ageBegin = 10;
+        Integer ageEnd = 20;
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        //判断字符串不为空，当Str为空白或者null时,isNotBlank返回false
+        if(StringUtils.isNotBlank(name)){
+            queryWrapper.like("name","n");
+        }
+        if(ageBegin != null){
+            queryWrapper.ge("age", ageBegin);
+        }
+        if(ageEnd != null){
+            queryWrapper.le("age", ageEnd);
+        }
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+    @Test
+    public void test8Condition() {
+        //定义查询条件，有可能为null（用户未输入）
+        String name = null;
+        Integer ageBegin = 10;
+        Integer ageEnd = 20;
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper
+                .like(StringUtils.isNotBlank(name), "name", "n")
+                .ge(ageBegin != null, "age", ageBegin)
+                .le(ageEnd != null, "age", ageEnd);
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+    @Test
+    public void test9() {
+        //定义查询条件，有可能为null（用户未输入）
+        String name = null;
+        Integer ageBegin = 10;
+        Integer ageEnd = 20;
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        //避免使用字符串表示字段，防止运行时错误,使用Lambda方式
+        queryWrapper
+                .like(StringUtils.isNotBlank(name), User::getName, "n")
+                .ge(ageBegin != null, User::getAge, ageBegin)
+                .le(ageEnd != null, User::getAge, ageEnd);
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+    /**
+     * 条件的优先级: 需求同Test7.在test7的基础上修改
+     * 查询名字中包含n，且（年龄小于18或email为空的用户），并将这些用户的年龄设置为18，邮箱设置为 user@ms.com
+     */
+    @Test
+    public void test10() {
+        //组装set子句
+        LambdaUpdateWrapper<User> updateWrapper = new LambdaUpdateWrapper<>();
+        //i代表的就是updateWrapper
+        updateWrapper
+                .set(User::getAge,18)
+                .set(User::getEmail,"user@ms.com")
+                .like(User::getEmail,"n")
+                .and(i -> i.lt(User::getAge,18).or().isNull(User::getEmail));
+        //这里必须要创建User对象，否则无法应用自动填充。如果没有自动填充，可以设置为null
+        User user = new User();
+        int result = userMapper.update(user, updateWrapper);
+        System.out.println(result);
+    }
 }
